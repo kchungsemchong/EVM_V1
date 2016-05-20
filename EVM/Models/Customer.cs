a3c9c26564c1e2cd28db1ec206b069eb3bb06126
kchungsemchong <kchungsemchong@gmail.com>
2016-05-20 20:12:21 +0400

    Add Missing Classes

---
+++ EVM/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EVM.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string UserId { get; set; }
+    }
+}
