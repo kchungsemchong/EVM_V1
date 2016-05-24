ec33aa9316088e0cfd9b9bfe81795dfda8b525d3
kchungsemchong <kchungsemchong@gmail.com>
2016-05-24 15:21:18 +0400

    Add Helper class

---
+++ EVM/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace EVM
+{
+    public class Helper
+    {
+        public string ConvertToTitleCase(string name)
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            name = textInfo.ToTitleCase(name);
+
+            return name;
+        }
+    }
+}
