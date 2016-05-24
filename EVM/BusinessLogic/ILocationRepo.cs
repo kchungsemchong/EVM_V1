b96485a5babc8361d1835a6afb27e32559982ae5
kchungsemchong <kchungsemchong@gmail.com>
2016-05-24 15:19:17 +0400

    Add ILocationRepo

---
+++ EVM/BusinessLogic/ILocationRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EVM.Models;
+
+namespace EVM.BusinessLogic
+{
+    public interface ILocationRepo
+    {
+        IEnumerable<LocationRepo> Retrieve();
+        LocationRepo Get(int id);
+        LocationRepo Create(LocationRepo item);
+        LocationRepo Update(LocationRepo item);
+    }
+}
