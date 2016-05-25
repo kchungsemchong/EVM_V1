7b228686ed48ad21f905c075703501cb4b666581
kchungsemchong <kchungsemchong@gmail.com>
2016-05-25 16:54:20 +0400

    Add LocationsController

---
+++ EVM/Controllers/LocationsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EVM.BusinessLogic;
+using EVM.Models;
+
+namespace EVM.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private readonly ILocationRepo _repo;
+
+        public LocationsController(ILocationRepo repository)
+        {
+            _repo = repository;
+        }
+
+        // GET: Locations
+        public ActionResult Index()
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    var records = _repo.Retrieve();
+                    if (records == null)
+                        return RedirectToAction("Error404", "Home");
+
+                    return View(records);
+                }
+
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // GET: Locations/Details/5
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    if (id > 0)
+                    {
+                        var record = _repo.Get(id);
+                        if (record.LocationId < 1)
+                            return RedirectToAction("Error404", "Home");
+
+                        return View(record);
+                    }
+
+                    return RedirectToAction("Error404", "Home");
+                }
+
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // GET: Locations/Create
+        public ActionResult Create()
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    return View();
+                }
+
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // POST: Locations/Create
+        [HttpPost]
+        public ActionResult Create(Location item)
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    var newRecord = _repo.Create(item);
+                    if (newRecord.LocationId < 1)
+                        return RedirectToAction("Error404", "Home");
+
+                    return RedirectToAction("Index", "Locations");
+                }
+
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // GET: Locations/Edit/5
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    if (id > 0)
+                    {
+                        var record = _repo.Get(id);
+
+                        return View(record);
+                    }
+                }
+
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // POST: Locations/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Location item)
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    if (item.LocationId > 0)
+                    {
+                        var updatedRecord = _repo.Update(item);
+                        if (updatedRecord.LocationId < 1)
+                            return RedirectToAction("Error404", "Home");
+
+                        return RedirectToAction("Index", "Locations");
+                    }
+                }
+
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+    }
+}
