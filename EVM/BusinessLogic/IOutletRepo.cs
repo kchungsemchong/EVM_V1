70e7a86b7ab863d457a5b3d28fe9542e00ad2b8b
kchungsemchong <kchungsemchong@gmail.com>
2016-05-26 07:39:51 +0400

    Add Outlet Business Logic

---
+++ EVM/BusinessLogic/IOutletRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EVM.Models;
+
+namespace EVM.BusinessLogic
+{
+    public interface IOutletRepo
+    {
+        IEnumerable<Outlet> Retrieve();
+        Outlet Get(int id);
+        Outlet Create(Outlet item);
+        Outlet Update(Outlet item);
+    }
+}
---
+++ EVM/BusinessLogic/OutletRepo.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EVM.Models;
+
+namespace EVM.BusinessLogic
+{
+    public class OutletRepo
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+        private readonly Helper helper = new Helper();
+
+        public IEnumerable<Outlet> Retrieve()
+        {
+            var records = (from outlet in db.Outlets
+                           where outlet.Status == "Active"
+                           select outlet).ToList();
+
+            return records;
+        }
+        public Outlet Get(int id)
+        {
+            var record = (from outlet in db.Outlets
+                          where outlet.OutletId == id
+                          select outlet).FirstOrDefault();
+
+            return record;
+        }
+        public Outlet Create(Outlet item)
+        {
+            bool duplicateExists = true;
+            item.City = helper.ConvertToTitleCase(item.City);
+            item.Name = helper.ConvertToTitleCase(item.Name);
+            duplicateExists = checkForDuplicates(item.Name);
+            if (duplicateExists == true)
+                return item;
+
+            db.Outlets.Add(item);
+            db.SaveChanges();
+
+            return item;
+        }
+        public Outlet Update(Outlet item)
+        {
+            var recordToUpdate = Get(item.OutletId);
+            recordToUpdate.City = item.City;
+            recordToUpdate.Street = item.Street;
+            recordToUpdate.Name = item.Name;
+
+            if (item.Status == "Archived")
+            {
+                recordToUpdate.Status = "Archived";
+                db.SaveChanges();
+
+                return item;
+            }
+            if (item.Status == "Active")
+            {
+                recordToUpdate.Status = "Active";
+                db.SaveChanges();
+
+                return item;
+            }
+
+            return item;
+        }
+
+        public bool checkForDuplicates(string name)
+        {
+            var records = (from n in db.Outlets
+                           where n.Name == name
+                           select n).ToList();
+            if (records.Count > 0)
+                return true;
+
+            return false;
+        }
+    }
+}
