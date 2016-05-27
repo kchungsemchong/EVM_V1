59a64ff57c98c9005ecbdfdea52fe348c58d80af
kchungsemchong <kchungsemchong@gmail.com>
2016-05-27 14:09:53 +0400

    Add Event Expiration Management

---
+++ EVM/App_Start/InitializeData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EVM.Models;
+
+namespace EVM.App_Start
+{
+    public class InitializeData
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+        public void InitializeApplicationData()
+        {
+            InitializeEventStatus();
+        }
+
+        public void InitializeEventStatus()
+        {
+            var records = (from ev in db.Events
+                           where ev.Status == "Active"
+                           select ev).ToList();
+
+            foreach (var item in records)
+            {
+                int differenceBetweenTwoDates = DateTime.Compare(Convert.ToDateTime(DateTime.UtcNow),
+                    Convert.ToDateTime(item.EventDate));
+                if (differenceBetweenTwoDates > 0)
+                {
+                    item.Status = "Archived";
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
--- EVM/Global.asax.cs
+++ EVM/Global.asax.cs
@@ -5,17 +5,21 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using EVM.App_Start;
 
 namespace EVM
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private InitializeData initData = new InitializeData();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            initData.InitializeApplicationData();
         }
     }
 }
