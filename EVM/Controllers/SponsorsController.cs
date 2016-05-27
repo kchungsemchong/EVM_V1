d12da36edc0f2708bcfe021c77351eab405084c7
kchungsemchong <kchungsemchong@gmail.com>
2016-05-27 06:56:51 +0400

    Add SponsorsController

---
+++ EVM/Controllers/SponsorsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EVM.BusinessLogic;
+using EVM.Models;
+
+namespace EVM.Controllers
+{
+    public class SponsorsController : Controller
+    {
+        private readonly ISponsorRepo _repo;
+
+        public SponsorsController(ISponsorRepo repository)
+        {
+            _repo = repository;
+        }
+
+        // GET: Sponsors
+        public ActionResult Index()
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    var records = _repo.Retrieve();
+                    if (records == null)
+                        return RedirectToAction("Error404", "Home");
+
+                    return View(records);
+                }
+
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // GET: Sponsors/Details/5
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    if (id > 0)
+                    {
+                        var record = _repo.Get(id);
+                        if (record.SponsorId < 1)
+                            return RedirectToAction("Error404", "Home");
+
+                        return View(record);
+                    }
+
+                    return RedirectToAction("Error404", "Home");
+                }
+
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // GET: Sponsors/Create
+        public ActionResult Create()
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    return View();
+                }
+
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // POST: Sponsors/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Sponsor item)
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    if (ModelState.IsValid)
+                    {
+                        var newRecord = _repo.Create(item);
+                        if (newRecord.SponsorId < 1)
+                            return RedirectToAction("Error404", "Home");
+
+                        return RedirectToAction("Index", "Sponsors");
+                    }
+                }
+
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // GET: Sponsors/Edit/5
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    if (id > 0)
+                    {
+                        var record = _repo.Get(id);
+
+                        return View(record);
+                    }
+                }
+
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+
+        // POST: Sponsors/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Sponsor item)
+        {
+            try
+            {
+                if (User.IsInRole("Admin"))
+                {
+                    if (ModelState.IsValid)
+                    {
+                        if (item.SponsorId > 0)
+                        {
+                            var updatedRecord = _repo.Update(item);
+                            if (updatedRecord.SponsorId < 1)
+                                return RedirectToAction("Error404", "Home");
+
+                            return RedirectToAction("Index", "Sponsors");
+                        }
+                    }
+                }
+
+                return RedirectToAction("Login", "Account");
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.InternalServerError, ex.ToString());
+            }
+        }
+    }
+}
