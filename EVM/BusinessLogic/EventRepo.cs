729402b1991502882a7e821673b2727badaaac28
kchungsemchong <kchungsemchong@gmail.com>
2016-05-27 07:12:46 +0400

    Add Business Logic For Event

---
+++ EVM/BusinessLogic/EventRepo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EVM.Models;
+
+namespace EVM.BusinessLogic
+{
+    public class EventRepo : IEventRepo
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+        private readonly Helper helper = new Helper();
+
+        public IEnumerable<Event> Retrieve()
+        {
+            var records = (from ev in db.Events
+                           where ev.Status == "Active"
+                           select ev).ToList();
+
+            return records;
+        }
+
+        public Event Get(int id)
+        {
+            var record = (from ev in db.Events
+                          where ev.EventId == id
+                          select ev).FirstOrDefault();
+
+            return record;
+        }
+
+        public Event Create(Event item)
+        {
+            return null;
+        }
+        public Event Update(Event item)
+        {
+            return null;
+        }
+    }
+}
---
+++ EVM/BusinessLogic/IEventRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EVM.Models;
+
+namespace EVM.BusinessLogic
+{
+    public interface IEventRepo
+    {
+        IEnumerable<Event> Retrieve();
+        Event Get(int id);
+        Event Create(Event item);
+        Event Update(Event item);
+    }
+}
