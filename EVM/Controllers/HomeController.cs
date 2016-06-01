de4f41ab414fd5749ae9f68e32d7d2d6147e5504
Satish Mohun <satishmohun@gmail.com>
2016-06-01 18:51:40 +0400

    Retrieve Event Details In EventViewModel

--- EVM/Controllers/HomeController.cs
+++ EVM/Controllers/HomeController.cs
@@ -11,7 +11,14 @@ namespace EVM.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IEventRepo _repo;
+
         private ApplicationDbContext db = new ApplicationDbContext();
+
+        public HomeController(IEventRepo repository)
+        {
+            _repo = repository;
+        }
 
         public ActionResult Index()
         {
@@ -44,10 +51,24 @@ namespace EVM.Controllers
 
         public ActionResult Events()
         {
-            var record = db.Events.Where(e => e.Status == "Active").ToList();
-            var photo = db.Photos.Where(p => p.PhotoId == 1).FirstOrDefault();
-            ViewBag.Photo = photo;
-            return View(record);
+            var records = _repo.Retrieve();
+
+            var eventList = new List<EventViewModel>();
+            foreach (var item in records)
+            {
+                var newEvent = new EventViewModel()
+                {
+                    EventId = item.EventId,
+                    Description = item.Description,
+                    EventDate = item.EventDate,
+                    Name = item.Name,
+                    LocationName = db.Locations.Where(l => l.LocationId == item.LocationId).FirstOrDefault().Name,
+                    WallpaperContent = db.Photos.Where(p => p.EventId == item.EventId).FirstOrDefault().Content
+                };
+                eventList.Add(newEvent);
+            }
+
+            return View(eventList);
         }
     }
 }
