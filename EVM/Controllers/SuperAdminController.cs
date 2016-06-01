370ddbf6ec57945ac5f343de7d6ba8ce66c6fe3a
Satish Mohun <satishmohun@gmail.com>
2016-06-01 22:36:58 +0400

    Add Controller For Super Admin

---
+++ EVM/Controllers/SuperAdminController.cs
@@ -0,0 +1,27 @@
+﻿using EVM.BusinessLogic;
+using EVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EVM.Controllers
+{
+    public class SuperAdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: SuperAdmin
+        public ActionResult Index()
+        {
+            if (User.IsInRole("Super"))
+            {
+                var records = db.Users.ToList();
+                return View(records);
+            }
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
